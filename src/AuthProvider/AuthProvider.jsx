39679b7f7e369e13f245ba793ddbd756a933530d
Tasinpronoy26 <tasinpronoy56@gmail.com>
2023-05-19 00:43:34 +0600

    Auth Provider

---
+++ src/AuthProvider/AuthProvider.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { getAuth } from "firebase/auth";
+
+
+
+const auth = getAuth(app);
+
+const AuthProvider = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default AuthProvider;
