49bee1c7469a42b9aaf27e625ee062e6b9ee1a8c
Tasinpronoy26 <tasinpronoy56@gmail.com>
2023-05-20 18:10:27 +0600

    My Toys Data Fetch

---
+++ src/Components/Pages/MyToys/MyInsertToy.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MyInsertToy = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default MyInsertToy;
--- src/Components/Pages/MyToys/MyToys.jsx
+++ src/Components/Pages/MyToys/MyToys.jsx
@@ -1,8 +1,27 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { AuthContext } from '../../../AuthProvider/AuthProvider';
 
 const MyToys = () => {
+    
+    const {users} = useContext(AuthContext);
+    const [ myToys, setMyToys ] = useState([]);
+
+
+    const url = `http://localhost:5000/mytoy?sellerEmail=${users.email}`
+
+    useEffect(() => {
+       
+        fetch(url)
+        .then(res => res.json())
+        .then(data => setMyToys(data))
+
+    },[])
+    
+
     return (
         <div>
+
+            <h1 className='text-center my-10 font-bold text-5xl'> My Toys </h1>
             
         </div>
     );
